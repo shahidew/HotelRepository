00ffe611bc55f51ac575abb00be0a93e73b6f494
shahidew <13sh78m@gmail.com>
2021-03-03 16:48:36 +0330

    Complete NationalCodeFilter page

--- src/main/java/filter/NationalCodeFilter.java
+++ src/main/java/filter/NationalCodeFilter.java
@@ -16,11 +16,17 @@ public class NationalCodeFilter implements Filter {
         response.setContentType("text/html");
         PrintWriter writer = response.getWriter();
         writer.print("<html><body>");
-        String nationalCode;
-        nationalCode = request.getParameter("pass");
+        String nationalCode, national_code, nationalNumber, national_number;
+        nationalCode = request.getParameter("nationalCode");
+        national_code = request.getParameter("national_code");
+        nationalNumber = request.getParameter("nationalNumber");
+        national_number = request.getParameter("national_number");
         Pattern pattern = Pattern.compile("[0-9]{10}");
         Matcher matcher = pattern.matcher(nationalCode);
-        if(matcher.matches()){
+        Matcher matcher1 = pattern.matcher(national_code);
+        Matcher matcher2 = pattern.matcher(nationalNumber);
+        Matcher matcher3 = pattern.matcher(national_number);
+        if(matcher.matches() || matcher1.matches() || matcher2.matches() || matcher3.matches()){
             chain.doFilter(request, response);
         }
         else{
