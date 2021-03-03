e018395f908a960468a1ba5820b6f32ae80ded6c
shahidew <13sh78m@gmail.com>
2021-03-03 17:55:05 +0330

    Complete CapacityFilter page

--- src/main/java/filter/CapacityFilter.java
+++ src/main/java/filter/CapacityFilter.java
@@ -3,14 +3,33 @@ package filter;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebFilter(filterName = "CapacityFilter")
 public class CapacityFilter implements Filter {
     public void destroy() {
     }
 
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        chain.doFilter(req, resp);
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.print("<html><body>");
+        String capacity;
+        capacity = request.getParameter("capacity");
+
+        Pattern pattern = Pattern.compile("[1-4]{1}");
+        Matcher matcher = pattern.matcher(capacity);
+        if(matcher.matches()){
+            chain.doFilter(request, response);
+        }
+        else{
+            writer.println("Error Capacity is not valid");
+            request.getRequestDispatcher("roomReservationSystem.html").include(request,response);
+        }
+        writer.print("</html></body>");
+        writer.close();
     }
 
     public void init(FilterConfig config) throws ServletException {
