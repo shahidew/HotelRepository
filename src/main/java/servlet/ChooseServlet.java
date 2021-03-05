5734a8e36eb99731841e2cf225a897789d0139a0
shahidew <13sh78m@gmail.com>
2021-03-05 20:10:42 +0330

    Complete Reserve

--- src/main/java/servlet/ChooseServlet.java
+++ src/main/java/servlet/ChooseServlet.java
@@ -7,10 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @WebServlet(name = "ChooseServlet")
 public class ChooseServlet extends HttpServlet {
@@ -18,36 +16,29 @@ public class ChooseServlet extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter writer = response.getWriter();
         writer.print("<html><body>");
-
         String op;
         op = request.getParameter("operation");
+
         if(op.equals("reserve")){
-            String name, family, nationalCode;
+            String name, family, nationalCode, startDateStr, endDateStr;
             int capacity, reserveCode;
+            reserveCode = 1001;
             name = request.getParameter("fname");
             family = request.getParameter("lname");
             nationalCode = request.getParameter("nationalCode");
-            String startDateStr = request.getParameter("start_date");
-            Date startDate = null;
-            try {
-                startDate = new SimpleDateFormat("yyyy/MM/dd").parse(startDateStr);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-            String endDateStr = request.getParameter("end_date");
-            Date endDate = null;
-            try {
-                endDate = new SimpleDateFormat("yyyy/MM/dd").parse(endDateStr);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
+            startDateStr = request.getParameter("start_date");
+            endDateStr = request.getParameter("end_date");
             capacity = Integer.parseInt(request.getParameter("capacity"));
-            reserveCode = 1001;
+            String[] arr = startDateStr.split("/");
+            servlet.Date startDate = new servlet.Date(Integer.parseInt(arr[0]),Integer.parseInt(arr[1]),Integer.parseInt(arr[2]));
+            String[] arr2 = endDateStr.split("/");
+            servlet.Date endDate = new Date(Integer.parseInt(arr2[0]),Integer.parseInt(arr2[1]),Integer.parseInt(arr2[2]));
             RoomReservationDao roomReservationDao = new RoomReservationDao();
-            RoomReservation roomReservation = new RoomReservation(name,family,nationalCode,startDate,endDate,capacity,roomReservationDao.returnCode(reserveCode));
+            reserveCode = roomReservationDao.returnCode(reserveCode);
+            RoomReservation roomReservation = new RoomReservation(name,family,nationalCode,startDate,endDate,capacity,reserveCode);
             roomReservationDao.insert(roomReservation);
             writer.println("the reserve is done " + "your reserve number is : " + roomReservation.getReserveCode());
-            writer.println(roomReservation.getName() + " " + roomReservation.getFamily() +" "+ roomReservation.getNationalCode()+" " + roomReservation.getStartDate() +" " + roomReservation.getEndDate() + " " + roomReservation.getCapacity() + " " + roomReservation.getReserveCode());
+           // writer.println(roomReservation.getName() + " " + roomReservation.getFamily() +" "+ roomReservation.getNationalCode()+" " + roomReservation.getStartDate() +" " + roomReservation.getEndDate() + " " + roomReservation.getCapacity() + " " + roomReservation.getReserveCode());
         }
         else if(op.equals("cancellation")){
 
